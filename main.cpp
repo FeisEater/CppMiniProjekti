6ba9940d1fe1d96d2def4f604a56aa262b577d80
Pavel Smirnov <pavelsmirnov94@gmail.com>
2014-11-16 03:04:19 +0200

    working on test driver

--- main.cpp
+++ main.cpp
@@ -11,52 +11,99 @@
 #include <exception>
 #include <stdexcept>
 #include <cstddef>
+#include <vector>
 
 using namespace std;
 
-static int testCounter;
 /*
  * 
  */
 
-void runTests();
-template <typename Type>
-Type check(Type realValue, Type expectedValue, string testName);
+#define test(testname) void testname() { curTestName = #testname;
 
-int main(int argc, char** argv) {
-    testCounter = 0;
-    try
-    {
-        runTests();
-    }
-    catch(exception const& e)
-    {
-        cout << " failed, code threw following exception:" << endl << e.what() << endl;
-    }
-    catch( ... )
-    {
-        cout << " failed, code threw unknown exception" << endl;
-    }
-    return 0;
-}
+template <typename Type>
+Type check(Type realValue, Type expectedValue);
+static string curTestName;
+
+class testfail: public exception
+{
+};
 
 float gimmeInt(bool fail)
 {
     if (fail)
-        return 5 / 0;
+        throw domain_error("arbitrary error");
     return 5;
 }
 
-void runTests()
+test(noErrors)
+    check<float>(gimmeInt(true), 5);
+}
+
+test(sixIsFive)
+    check<int>(5, 5);
+    check<int>(5, 6);
+}
+
+test(fiveIsFive)
+    check<int>(5, 5);
+    check<int>(4, 4);
+}
+
+test(vectorsAreEqual)
+    vector<int> v1 = {1,2,3};
+    vector<int> v2 = {1,2,3};
+    if (v1 != v2)
+    {
+        cout << curTestName << " failed" << endl;
+        throw testfail();
+    }
+    //check<vector<int>>(4, 4);
+}
+
+string gimmeString() {return NULL;}
+test(nullString)
+    check<string>(gimmeString(), "foo");
+}
+
+void runTests(int testCounter)
 {
-    check<float>(gimmeInt(true), 5, "test test");
+    vector<void (*)()> tests = {sixIsFive, noErrors, fiveIsFive, vectorsAreEqual, nullString};
+    try
+    {
+        for (; testCounter < tests.size(); testCounter++)
+        {
+            tests[testCounter]();
+            cout << curTestName << " passed" << endl;
+        }
+    }
+    catch(testfail const& e)
+    {
+        runTests(testCounter + 1);
+    }
+    catch(exception const& e)
+    {
+        cout << curTestName << " failed, code threw following exception: " << e.what() << endl;
+        runTests(testCounter + 1);
+    }
+    catch( ... )
+    {
+        cout << curTestName << " failed, code threw unknown exception" << endl;
+        runTests(testCounter + 1);
+    }
+}
+
+int main(int argc, char** argv) {
+    runTests(0);
+    return 0;
 }
 
 template <typename Type>
-Type check(Type realValue, Type expectedValue, string testName)
+Type check(Type realValue, Type expectedValue)
 {
-    if (realValue == expectedValue)
-        cout << testName << " passed" << endl;
-    else
-        cout << testName << " failed. Expected " << expectedValue << " but was " << realValue << endl;
+    if (realValue != expectedValue)
+    {
+        cout << curTestName << " failed" << endl;//: expected " << expectedValue << " but was " << realValue << endl;
+        throw testfail();
+    }
 }
